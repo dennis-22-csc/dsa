b018c7f2793cfca9c9900121954c3413369fb580
dennis-22-csc <dennisakpotaire@gmail.com>
2023-09-11 19:38:05 +0100

    two sum contd

--- two-sum.py
+++ two-sum.py
@@ -3,8 +3,9 @@ def c_print(text):
         file.write(f"{text}\n")
 
 prices = [30, 10, 40, 70, 60]
-
-for price in prices:
-    complement = 100 - price
-    if complement in prices:
-        print(f"{price} and {complement}")
+prices_dict = {}
+for i in range(len(prices)):
+    complement = 100 - prices[i]
+    prices_dict[prices[i]] = i
+    if complement in prices_dict:
+        print(f"{i} and {prices_dict[complement]}")
