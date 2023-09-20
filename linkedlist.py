70400ab1632c2629b3b1bf1191b4dc13a7e2bf39
dennis-22-csc <dennisakpotaire@gmail.com>
2023-09-20 14:49:40 +0100

    implemented deleting element given index

--- linkedlist.py
+++ linkedlist.py
@@ -18,6 +18,50 @@ class SinglyLinkedList:
         else:
             self.node = Node(node_data, self.node)
 
+    def add_last(self, node_data):
+        self.count += 1
+        if self.count <= 1:
+            self.node = Node(node_data, None)
+        else:
+            temp_node = self.node
+            # while temp_node.next_node is not None, switch to next node
+            while temp_node.next_node:
+                temp_node = temp_node.next_node
+            # when temp_node.next_node is None, change next_node
+            temp_node.next_node = Node(node_data, None)
+
+    def add_at_index(self, node_data, index):
+        temp_count = 1
+        temp_node = self.node
+        if index < 0 or index > self.count:
+            raise IndexError("Index out of range")
+        while temp_node.next_node:
+            if index == 0:
+                self.node = Node(node_data, self.node)
+                return
+            if index == temp_count:
+                temp_node.next_node = Node(node_data, temp_node.next_node)
+                return
+            temp_node = temp_node.next_node
+            temp_count += 1
+        temp_node.next_node = Node(node_data, temp_node.next_node)
+
+    def delete_at_index(self, index):
+        temp_count = 1
+        temp_node = self.node
+        if index < 0 or index >=self.count:
+            raise IndexError("Index out of range")
+        while temp_node.next_node:
+            if index == 0:
+                self.node = self.node.next_node
+                return
+            if index == temp_count:
+                temp_node.next_node = temp_node.next_node.next_node
+                return
+            temp_node = temp_node.next_node
+            temp_count += 1
+        temp_node.next_node = temp_node.next_node.next_node
+
     def __iter__(self):
         self.current_node = self.node
         return self
@@ -46,13 +90,14 @@ mylist.add(20)
 mylist.add(90)
 mylist.add(40)
 mylist.add(10)
+mylist.delete_at_index(0)
 
 # Test that the list is iterable
 for i in mylist:
     print(i)
 
 # Test that the list is indexable
-print(mylist[3])
+#print(mylist[3])
 
 # Test that an index out of range exception will be thrown
-print(mylist[4])
+#print(mylist[4])
