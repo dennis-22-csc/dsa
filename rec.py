68e4a5afbabd6e5003aefedfe5fde081c63a4932
dennis-22-csc <dennisakpotaire.gmail.com>
2023-10-20 09:46:21 +0100

    more recursion

--- rec.py
+++ rec.py
@@ -1,9 +1,16 @@
-x = 0
-def print_num(n):
-    global x
-    if n > 0:
-        x += 1
-        return print_num(n-1) + x
-    return 0
+def half_list(mylist, index, sublist1, sublist2):
+    if index < len(mylist):
+        if index < 5:
+            sublist1.append(mylist[index])
+        else:
+            sublist2.append(mylist[index])
+        half_list(mylist, index + 1, sublist1, sublist2)
 
-print("Result: ", print_num(10))
+employees = ["Dennis Koko", "Lincoln Burrow", "Michael Scofield", "Nathan Petrelli", "Peter Petrelli", "Sarah Connor", "John Connor", "Raymond Gray", "Ryan Whitewater", "Jessica Roberts"]
+print("Employees List: ", employees) 
+start_index = 0
+sublist1 = [] 
+sublist2 = [] 
+half_list(employees, start_index, sublist1, sublist2)
+print("Sub List 1: ", sublist1)
+print("Sub List 2: ", sublist2)
